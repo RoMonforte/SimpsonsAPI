fb9d0afda48bed00b9bc801eb0f3eabd39f5cb63
Rodrigo Monforte <rodrigomonforte94@gmail.com>
2022-10-28 04:25:45 -0500

    migrations

--- db/migrations/20221026071727-fixedaa-tables.js
+++ db/migrations/20221026071727-fixedaa-tables.js
@@ -21,17 +21,17 @@ module.exports = {
     await queryInterface.dropTable(EPISODE_LOCATION_TABLE);
 
     await queryInterface.dropTable(LOCATION_TABLE);
-    await queryInterface.dropTable(CHARACTER_TABLE);
     await queryInterface.dropTable(EPISODE_TABLE);
+    await queryInterface.dropTable(CHARACTER_TABLE);
 
     await queryInterface.createTable(CHARACTER_EPISODE_TABLE, CharacterEpisodeSchema);
     await queryInterface.createTable(CHARACTER_LOCATION_TABLE, CharacterLocationSchema);
     await queryInterface.createTable(EPISODE_LOCATION_TABLE, EpisodeLocationSchema);
 
-
-    await queryInterface.createTable(CHARACTER_TABLE, CharacterSchema);
-    await queryInterface.createTable(EPISODE_TABLE, EpisodeSchema);
     await queryInterface.createTable(LOCATION_TABLE, LocationSchema);
+    await queryInterface.createTable(EPISODE_TABLE, EpisodeSchema);
+    await queryInterface.createTable(CHARACTER_TABLE, CharacterSchema);
+
 
   },
 
